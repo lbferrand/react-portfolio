8a4976765702b1fb0db6f966619c77dc672e5d35
lbferrand <lbferrand@gmail.com>
2023-03-17 19:09:45 -0400

    Initial set up and navigation

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+import Sidebar from '../Sidebar'
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,66 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="Logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/luciana-bautista-ferrand-615bb0216/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+
+      <li>
+        <a href="https://github.com/lbferrand" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://www.linkedin.com/in/luciana-bautista-ferrand-615bb0216/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
