1d055a72139fd5fdaaec11624e44c57a265e0855
lbferrand <lbferrand@gmail.com>
2023-03-17 22:40:24 -0400

    Add style to layout and create home folder

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -11,6 +11,12 @@ const Layout = () => {
         <span className="tags top-tags">&lt;body&gt;</span>
 
         <Outlet />
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
       </div>
     </div>
   )
